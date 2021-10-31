e68499491b9ee383521e4db8dda5ee5be820281d
Jayki-ZX <danteantu5@gmail.com>
2021-10-31 00:50:51 -0300

    comments

--- getdata.py
+++ getdata.py
@@ -2,6 +2,17 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from pprint import pprint
 
+# todo hacerle un env + requirements
+# Por ahora getdata hace lo siguiente
+# - Simula una conexion a la pagina del mapa.
+# - Saca los datos ejecutando el comando return JSON.parse(window._pageData)[1][6][i][12][0][13][0] en la consola del navegador.
+# Donde "i" representa el numero de agrupación (limite de kW, AC/DC) asignado por enel (rojo = 0, morado = 1, verde = 2)
+# - Guarda el resultado del paso anterior en una lista de python.
+
+# selenium hace los siguientes parse
+# Los null de js pasan a None de python
+# Los true/false de js pasan a True/False de python
+
 options = webdriver.ChromeOptions()
 
 options.add_argument('headless')
@@ -11,12 +22,10 @@ browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 url = "https://www.google.com/maps/d/u/0/embed?mid=1WJH79QSYNRlQIBrkhFB8OQxXpDFG6L0q"
 browser.get(url)
 
-#print(browser.page_source)
 # _pageData[1][6][0][12][0][13][0]
 # _pageData[1][6][1][12][0][13][0]
 # _pageData[1][6][2][12][0][13][0]
-# Los null de js pasan a None de python
-# Los true/false de js pasan a True/False de python <3
+
 a = browser.execute_script("return JSON.parse(window._pageData)[1][6][0][12][0][13][0]")
 #b = browser.execute_script("return JSON.parse(window._pageData)[1][6][1][12][0][13][0]")
 #c = browser.execute_script("return JSON.parse(window._pageData)[1][6][2][12][0][13][0]")
